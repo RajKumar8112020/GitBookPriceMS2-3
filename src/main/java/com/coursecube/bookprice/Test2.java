887d448d5fc88740cb9e85d4d67268afab7d1813
RAJ KUMAR GUPTA <raj1sjce@gmail.com>
2022-02-13 20:16:29 -0800

    This is Test2 Commit...........

---
+++ src/main/java/com/coursecube/bookprice/Test2.java
@@ -0,0 +1,17 @@
+package com.coursecube.bookprice;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String  str="Arvind";
+		
+		if(str == "Arvind" && str ==null) {
+			System.out.println("Welcome Arvind.........");
+		}
+		else
+		{
+			System.out.println("Do Work Arvind .........");
+		}
+	}
+
+}
